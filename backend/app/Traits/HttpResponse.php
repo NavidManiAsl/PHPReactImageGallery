f367ca6e78b433cb82ad103cb4a5c09b4164710f
NavidManiAsl <navid.maniasl@outlook.com>
2023-11-13 08:43:41 +0100

    Create Httpresponse trait

---
+++ backend/app/Traits/HttpResponse.php
@@ -0,0 +1,22 @@
+<?php
+
+trait HttpResponse {
+
+    public function success ($data, $message=null, $code=200){
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => $message, 
+            'data' => $data
+        ],$code);
+    }
+
+    public function error ($data, $message, $code){
+
+        return response()->json([
+            'status' => 'error',
+            'message' => $message, 
+            'data' => $data
+        ],$code);
+    }
+}
