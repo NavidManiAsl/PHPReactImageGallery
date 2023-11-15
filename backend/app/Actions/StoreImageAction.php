f2f0a97d00624888b4777d752b3f9bf457144ef6
NavidManiAsl <navid.maniasl@outlook.com>
2023-11-16 00:04:25 +0100

    Create StoreImageAction class

--- backend/app/Actions/StoreImageAction.php
+++ backend/app/Actions/StoreImageAction.php
@@ -1,8 +1,43 @@
 <?php
 namespace App\Actions;
-class StoreImageAction {
 
-    public function __invoke() { 
+use App\Http\Requests\StoreImageRequest;
+use App\Models\Image;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Intervention\Image\Facades\Image as InterventionImage;
+
+class StoreImageAction
+{
+
+    public function __invoke(StoreImageRequest $request)
+    {
+        $image = new Image();
+        $file = $request->file("image");
+        $thumb = InterventionImage::make($file)->resize(300,300)->save(storage_path('storage').$file->getClientOriginalName());
+        $hashedName = $file->store();
+        $fileDimension = $file->dimensions()[0] . ' x ' . $file->dimensions()[1];
         
-       }
-};
+        $image->name = $file->getClientOriginalName();
+        $image->path = storage_path() .'/'. $hashedName;
+        $image->thumbnail_path = $thumb->basePath();
+        $image->description = $request->get('description');
+        $image->size = $file->getSize();
+        $image->dimension = $fileDimension;
+        $image->tags = $request->get('tags');
+        $image->user_id = Auth::guard('sanctum')->user()->id;
+        $image->gallery_id = $request->gallery ?
+            $request->gallery
+            : 1;
+          dd($image);
+    try {
+        $image->save();
+        return true;
+    } catch (\Throwable $th) {
+        Log::error($th->getMessage());
+    };
+    
+    }
+
+}
+;
