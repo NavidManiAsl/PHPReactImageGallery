701406b2a11e4038548230f813967dd0159e0478
NavidManiAsl <navid.maniasl@outlook.com>
2023-11-23 21:48:51 +0100

    Create request validation and make the tags optional

---
+++ backend/app/Http/Requests/StoreGalleryRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Rules\Serialized;
+
+class StoreGalleryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'=>['string', 'min:3', 'max:256','required'],
+            'tags' => ['string','max:2000', new Serialized]
+        ];
+    }
+}
