b624742c88b8341b3b8e0d42bec25c7ecbb952fe
NavidManiAsl <navid.maniasl@outlook.com>
2023-11-23 23:11:16 +0100

    Creat index method

--- backend/app/Http/Controllers/GalleryController.php
+++ backend/app/Http/Controllers/GalleryController.php
@@ -11,6 +11,11 @@ use Illuminate\Support\Facades\Log;
 class GalleryController extends Controller
 {
     use HttpResponse;
+   
+    
+    /**
+     * Store a newly created resource in storage.
+     */
     public function store(StoreGalleryRequest $request)
     {
       
@@ -28,7 +33,25 @@ class GalleryController extends Controller
             return $this->success($gallery, 'Gallery has been created successfully', 200);
         } catch (\Throwable $th) {
             Log::error($th->getMessage());
-            return $this->error(null,'Unexpected Error', 500);
+            return $this->serverError();
+        }
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request){
+        
+        $user = $request->user('sanctum');
+        if(!$user) {
+            return $this->unauthenticated();
+        }
+
+        try {
+            $galleries = Gallery::where('user_id', $user->id)->get();
+            return $this->success($galleries,'ok',200);
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+            return $this->serverError();
         }
     }
 }
