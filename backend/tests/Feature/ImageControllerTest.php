850ebccfce8f430a1b7a576a1b3cf7431b10c011
NavidManiAsl <navid.maniasl@outlook.com>
2023-11-23 10:53:29 +0100

    Create tests for imagecontroller show method

--- backend/tests/Feature/ImageControllerTest.php
+++ backend/tests/Feature/ImageControllerTest.php
@@ -11,7 +11,7 @@ class ImageControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_index_method_send_401_to_guest_call()
+    public function test_index_method_return_401_to_guest_call()
     {
         $response = $this->get("/api/v1/images");
         $response->assertStatus(401);
@@ -45,5 +45,46 @@ class ImageControllerTest extends TestCase
         $data = json_decode($response->getContent(), true);
         $this->assertEquals($imageA->id, $data['data'][0]['id']);
         $this->assertNotEquals($imageB->id, $data['data'][0]['id']);
+        auth()->logout();
+    }
+
+    public function test_show_method_return_401_to_guest_call()
+    {
+        $image = Image::factory()->create();
+        $response = $this->get('/api/v1/images/'. $image->id);
+        $response->assertStatus(401);
+    }
+
+    public function test_show_method_show_an_Image()
+    {
+        $user = User::factory()->create();
+        auth()->login($user);
+        $image = Image::factory()->create(['user_id'=> $user->id]);
+        $response = $this->get('/api/v1/images/'. $image->id);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'status',
+            'message',
+            'data'=>[],
+        ]);
+        
+        auth()->logout();
+    }
+    public function test_show_method_show_user_image_to_authorized_call()
+    {
+        $userA = User::factory()->create();
+        $userB = User::factory()->create();
+        $imageA = Image::factory()->create(['user_id' => $userA->id]);
+        $imageB = Image::factory()->create(['user_id' => $userB->id]);
+        auth()->login($userA);
+
+        $response = $this->get('/api/v1/images/'. $imageB->id);
+        $response->assertStatus(401);
+        $response->assertJsonStructure([
+            'status',
+            'message',
+            'data',
+        ]);
+
     }
 }
