2dea58513d7e863ed368d4f9eb246c5ebafa562a
NavidManiAsl <navid.maniasl@outlook.com>
2023-11-24 12:53:59 +0100

    Create show method for galleries

--- backend/app/Http/Controllers/GalleryController.php
+++ backend/app/Http/Controllers/GalleryController.php
@@ -11,19 +11,19 @@ use Illuminate\Support\Facades\Log;
 class GalleryController extends Controller
 {
     use HttpResponse;
-   
-    
+
+
     /**
      * Store a newly created resource in storage.
      */
     public function store(StoreGalleryRequest $request)
     {
-      
+
         if (!$request->user('sanctum')) {
             return $this->unauthenticated();
         }
         $gallery = Gallery::create([
-            "name"=> $request->name,
+            "name" => $request->name,
             "tags" => $request->tags,
             "user_id" => $request->user('sanctum')->id,
         ]);
@@ -39,19 +39,35 @@ class GalleryController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request){
-        
+    public function index(Request $request)
+    {
+
         $user = $request->user('sanctum');
-        if(!$user) {
+        if (!$user) {
             return $this->unauthenticated();
         }
 
         try {
             $galleries = Gallery::where('user_id', $user->id)->get();
-            return $this->success($galleries,'ok',200);
+            return $this->success($galleries, 'ok', 200);
         } catch (\Throwable $th) {
             Log::error($th->getMessage());
             return $this->serverError();
+        }
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Gallery $gallery)
+    {
+
+        try {
+            return $this->success($gallery, 'ok', 200);
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+            return $this->error(null, 'not Found', 404);
         }
     }
 }
