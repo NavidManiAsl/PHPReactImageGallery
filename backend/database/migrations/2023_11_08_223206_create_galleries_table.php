db7780caaa4705f10a797433fa04f5e977c3be67
NavidManiAsl <navid.maniasl@outlook.com>
2023-11-24 13:04:13 +0100

    Add images column to galleries table

--- backend/database/migrations/2023_11_08_223206_create_galleries_table.php
+++ backend/database/migrations/2023_11_08_223206_create_galleries_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('galleries', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('images')->nullable();
             $table->string('tags')->nullable();
             $table->foreignId('user_id')
                 ->constrained()
