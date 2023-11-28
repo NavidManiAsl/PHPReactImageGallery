7d1ccd982ae147e25a2246986b8d53ece9b69b03
NavidManiAsl <navid.maniasl@outlook.com>
2023-11-28 20:53:43 +0100

    Create mutator to deal with image tags serialization

--- backend/app/Models/Image.php
+++ backend/app/Models/Image.php
@@ -2,12 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the image tags.
+     */
+    protected function tags():Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => unserialize($value),
+            set: fn ($value) => serialize($value));
+    }
 
     public function user()
     {
