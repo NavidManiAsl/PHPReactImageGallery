84827d21657e956c01822767054b972c2ad0573d
NavidManiAsl <navid.maniasl@outlook.com>
2023-11-28 21:44:05 +0100

    Add checkownership middleware to add, remove tags

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\{AuthController, ImageController, GalleryController};
 use App\Http\Middleware\Auth;
 use App\Http\Middleware\CheckOwnership;
+use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
 
@@ -15,6 +16,11 @@ Route::middleware(Auth::class)->prefix('/images')->group(function () {
 
     Route::post('/', [ImageController::class, 'store']);
     Route::get('/', [ImageController::class, 'index']);
+
+    Route::post('/{image}/tags', [ImageController::class,'addTags'])
+    ->middleware(CheckOwnership::class);
+    Route::delete('/{image}/tags', [ImageController::class,'removeTags'])
+        ->middleware(CheckOwnership::class);
 
     Route::get('/{image}', [ImageController::class, 'show'])
         ->middleware(CheckOwnership::class);
