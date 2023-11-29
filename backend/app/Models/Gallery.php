4eebf59f7c348c4e53fcff17dffb7288e1425dbc
NavidManiAsl <navid.maniasl@outlook.com>
2023-11-29 22:18:35 +0100

    use accessor for galleries tags

--- backend/app/Models/Gallery.php
+++ backend/app/Models/Gallery.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +10,13 @@ class Gallery extends Model
     use HasFactory;
 
     public $guarded = [];
+
+    protected function tags():Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value ? unserialize($value):[],
+            set: fn ($value) => serialize($value));
+    }
 
     public function user()
     {
