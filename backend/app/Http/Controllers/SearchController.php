ab80fe15c9451261bc19ec74bf34d14d110f3777
NavidManiAsl <navid.maniasl@outlook.com>
2023-12-01 13:27:14 +0100

    Implement DI in class

--- backend/app/Http/Controllers/SearchController.php
+++ backend/app/Http/Controllers/SearchController.php
@@ -11,20 +11,27 @@ use Illuminate\Support\Facades\Log;
 class SearchController extends Controller
 {
     use HttpResponse;
-    public function search(SearchRequest $request, ImageController $imageController, GalleryController $galleryController)
+    private $imageController;
+    private $galleryController;
+    public function __construct(ImageController $imageController, GalleryController $galleryController){
+        $this->imageController = $imageController;
+        $this->galleryController = $galleryController;
+    }
+    
+    public function search(SearchRequest $request )
     {
         $user = $request->user('sanctum')->id;
         $query = htmlspecialchars($request->input("query"));
 
         try {
-            $images = $imageController->search($query, $user);
-            $galleries = $galleryController->search($query, $user);
+            $images = $this->imageController->search($query, $user);
+            $galleries = $this->galleryController->search($query, $user);
         } catch (\Exception $e) {
             Log::error($e->getMessage());
             return $this->serverError();
         }
         if(empty($images) && empty($galleries)) {
-            return $this->error(null, 'Not Found', 404);
+            return $this->error(null, 'Not Found', 204);
         }
 
         $searchResult = ['images' => $images, 'galleries' => $galleries];
