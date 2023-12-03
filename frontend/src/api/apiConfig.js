2e7f492e4a443ea3fa6f1b694a21cc5f8c2b9899
NavidManiAsl <navid.maniasl@outlook.com>
2023-12-03 16:22:26 +0100

    Store api related constants

---
+++ frontend/src/api/apiConfig.js
@@ -0,0 +1,13 @@
+const API_BASE_URL = 'http://127.0.0.1:8000/api/v1/'
+
+const API_ENDPOINTS = {
+    login: 'login',
+    logout: 'logout',
+    register: 'register',
+}
+
+export const API_URLS = {
+    login: `${API_BASE_URL}${API_ENDPOINTS.login}`,
+    logout: `${API_BASE_URL}${API_ENDPOINTS.logout}`
+    
+}
