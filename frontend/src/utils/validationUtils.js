f7523b79dbccb2b016db1c774a6725f1a1d23a23
NavidManiAsl <navid.maniasl@outlook.com>
2023-12-04 23:40:15 +0100

    Refactor return an object instead of bool

--- frontend/src/utils/validationUtils.js
+++ frontend/src/utils/validationUtils.js
@@ -1,25 +1,22 @@
 export const validateUsername = (username) => {
-    username = username.trim();
-    if (!username || username.length < 3) {
-        return false;
-    }
-    return true;
+    username = username?.trim();
+    return { status: username && username.length > 3, message:'username must be at least 3 characters' }
 };
 
 
 export const validatePassword = (password, passwordConfirmation = null) => {
-    if(!password || password.length < 8){
-        return false;
+    if (!password || password.length < 8) {
+        return { status: false, message: 'Password must be at least 8 characters' };
     }
     if (passwordConfirmation) {
-        if(password !== passwordConfirmation){
-            return false
+        if (password !== passwordConfirmation) {
+            return { status: false, message: 'Password and confirmation are not match' };
         }
     }
     return true
 };
 export const validateEmail = (email) => {
-    email = email.trim();
-    let regex = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
-    return regex.test(email);
+    email = email?.trim();
+    let regex = /^[a-z0-9]+@[a-z]+\.[a-z0-9]{2,3}$/;
+    return {status:regex.test(email), message:'invalid email'};
 };
