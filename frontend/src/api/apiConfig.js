84e663868b9ce9ddbb3f1ae2eaac9b37c9c4c142
NavidManiAsl <navid.maniasl@outlook.com>
2023-12-09 21:18:20 +0100

    Export baseurl and apiendpoints

--- frontend/src/api/apiConfig.js
+++ frontend/src/api/apiConfig.js
@@ -1,6 +1,6 @@
-const API_BASE_URL = 'http://127.0.0.1:8000/api/v1/'
+export const API_BASE_URL = 'http://127.0.0.1:8000/api/v1/'
 
-const API_ENDPOINTS = {
+export const API_ENDPOINTS = {
     login: 'login',
     logout: 'logout',
     register: 'register',
