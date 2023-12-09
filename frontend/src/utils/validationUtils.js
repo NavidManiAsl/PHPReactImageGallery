7100b4d8afa0dae329c8a0156c1a585392f2402c
NavidManiAsl <navid.maniasl@outlook.com>
2023-12-09 23:51:24 +0100

    Move validateRegisterdata from Register to here

--- frontend/src/utils/validationUtils.js
+++ frontend/src/utils/validationUtils.js
@@ -1,6 +1,6 @@
 export const validateUsername = (username) => {
     username = username?.trim();
-    return { status: username.length >0 && username.length > 3, message: 'username must be at least 3 characters' }
+    return { status: username.length > 0 && username.length > 3, message: 'username must be at least 3 characters' }
 };
 
 
@@ -17,6 +17,31 @@ export const validatePassword = (password, passwordConfirmation = null) => {
 };
 export const validateEmail = (email) => {
     email = email?.trim();
-    let regex = /^[a-z0-9]+@[a-z]+\.[a-z0-9]{2,3}$/;
+    let regex = /^[a-z0-9]?.+@[a-z]+\.[a-z0-9]{2,3}$/;
     return { status: regex.test(email), message: 'invalid email' };
+};
+
+export const validateRegisterData = (username, email, password, passwordConfirm) => {
+    const error = { username: '', email: '', password: '', passwordConfirm: '' }
+    const { status: validUsername, message: usernameError } = validateUsername(username);
+if(!validUsername) error.username = usernameError;
+
+    const { status: validEmail, message: emailError } = validateEmail(email);
+
+   if(!validEmail) error.email=emailError;
+
+    const { belongsTo: belongsTo, status: validPassword, message: passwordError } = validatePassword(password, passwordConfirm);
+
+    if(!validPassword && belongsTo=='password') {
+        error.password = passwordError;
+    }
+    if(!validPassword && belongsTo=='passwordConfirm') {
+        error.passwordConfirm = passwordError;
+    }
+  
+    
+    if(!error.username &&!error.password &&!error.passwordConfirm &&!error.email) return false;
+        
+      
+        return error;
 };
