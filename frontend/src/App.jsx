1c304ee40da7f8e68616d2237b6333b313b5b734
NavidManiAsl <navid.maniasl@outlook.com>
2023-12-10 20:19:29 +0100

    Create main route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,25 @@
 import { useState } from "react";
 import Home from "./pages/Home";
+import Main from "./pages/Main"
 import Error from "./pages/Error";
 import Login from "./pages/auth/Login";
-import Register from './pages/auth/Register'
+import Register from "./pages/auth/Register";
 import "./App.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home />, errorElement: <Error /> },
   { path: "/login", element: <Login />, errorElement: <Error /> },
-  { path: "/register", element: <Register />, errorElement: <Error /> }
-
+  { path: "/register", element: <Register />, errorElement: <Error /> },
+  { path: "/main", element: <Main />, errorElement: <Error /> },
 ]);
 
 function App() {
-  return <>
-  <RouterProvider router={router}/>
-  </>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
