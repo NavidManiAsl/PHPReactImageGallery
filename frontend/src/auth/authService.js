6e8a7ae2778f3fb667b73b1bfa8a169a36da0d56
NavidManiAsl <navid.maniasl@outlook.com>
2023-12-10 20:22:26 +0100

    Create register function

--- frontend/src/auth/authService.js
+++ frontend/src/auth/authService.js
@@ -1,13 +1,18 @@
 import {apiPost} from '../api/api';
 import  {API_URLS}  from '../api/apiConfig';'../api/apiConfig'
 
-export const login = async (email, password) => 
+export const login = async (formData) => 
 {
+    const{email,password} = formData
     const payload = ({email:email, password:password})
     return await apiPost(API_URLS.login,payload);
      
 } 
 
-export const register = async (email ,password,)=>{
-
+export const register = async (formData)=>{
+    const {username, email, password,passwordConfirmation} = formData;
+    const payload = ({name:username, email:email, password:password, password_confirmation:passwordConfirmation})
+    console.log(payload)
+    const result = await apiPost(API_URLS.register,payload);
+    console.log(result)
 }
